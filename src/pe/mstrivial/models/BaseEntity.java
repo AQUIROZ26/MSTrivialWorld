02259e352786c44b9756ebae549077e1c799599b
Aaron Quiroz Hostia <1412955@utp.edu.pe>
2018-02-10 10:38:08 -0500

    Clase BaseEntity:
    Clase Padre para las demás Entity.

---
+++ src/pe/mstrivial/models/BaseEntity.java
@@ -0,0 +1,15 @@
+package pe.mstrivial.models;
+
+import java.sql.Connection;
+
+public class BaseEntity {
+    private Connection connection;
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public void setConnection(Connection connection) {
+        this.connection = connection;
+    }
+}
