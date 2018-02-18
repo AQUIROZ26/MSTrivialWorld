48b4ebe3fb7f6780e8f4f9f51e768d70adac27da
Aaron Quiroz Hostia <1412955@utp.edu.pe>
2018-02-18 04:49:10 -0500

    Se avanzó con el TwService,lo que ayudará a los JSF.

--- src/pe/mstrivial/models/TwService.java
+++ src/pe/mstrivial/models/TwService.java
@@ -5,6 +5,7 @@ import javax.naming.NamingException;
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Date;
 import java.util.List;
 
 public class TwService {
@@ -63,5 +64,34 @@ public class TwService {
         return getPeopleEntity() != null ? getPeopleEntity().findById(id) : null;
     }
 
+    public Person findPersonByFirstName (String firstName){
+        return getPeopleEntity() != null ? getPeopleEntity().findByFirstName(firstName) : null ;
+    }
 
+    public Person findPersonByLastNameP (String lastNameP){
+        return getPeopleEntity() != null ? getPeopleEntity().findByLastNameP(lastNameP) : null;
+    }
+
+    public  Person findPersonByLastNameM (String lastNameM){
+        return  getPeopleEntity() != null ? getPeopleEntity().findByLastNameM(lastNameM) : null;
+    }
+
+    public Person findPersonByEmail (String email){
+        return  getPeopleEntity() != null ? getPeopleEntity().findByEmail(email) : null;
+    }
+
+    public  Person createPerson (int id_company, String firstName, String lastNameP, String lastNameM, String country,
+                                 String email, String username, String password, Date createDate, Date modifyDate){
+        return getPeopleEntity() != null ? getPeopleEntity()
+                .create(id_company,firstName,lastNameP,lastNameM, country,
+                        email, username,password,createDate,modifyDate) : null;
+    }
+
+    public boolean deletePerson(int id){
+        return getPeopleEntity() != null ? getPeopleEntity().delete(id) : false;
+    }
+
+    public boolean updatePerson (Person person) {
+            return getPeopleEntity() != null ? getPeopleEntity().updateFirstName(person) : false;
+    }
 }
