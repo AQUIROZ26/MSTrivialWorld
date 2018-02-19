254484e6b88fb46117a4e4987e10f8bbed005227
DanitzaLlanos <35356156+danitzallanos@users.noreply.github.com>
2018-02-19 08:49:23 -0500

    Se realizo la clase login de trivias

---
+++ src/pe/edu/utp/Login.java
@@ -0,0 +1,33 @@
+package pe.edu.utp;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+import java.io.Serializable;
+
+@Named(value = "login")
+@SessionScoped
+public class Login implements Serializable {
+    private String userName;
+    private String password;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String doValidateInput(){
+        if (!"sample".equals(password))return "error";
+        return "success";
+    }
+}
