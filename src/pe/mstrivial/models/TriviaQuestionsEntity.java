e92b76947cff8b1b1b6b38f8bce4453fb90a6f08
Aaron Quiroz Hostia <1412955@utp.edu.pe>
2018-02-24 17:31:54 -0500

    Se ha hecho las mejoras en TriviasEntity y se està actualizando la clase TriviaQuestionsEntity

---
+++ src/pe/mstrivial/models/TriviaQuestionsEntity.java
@@ -0,0 +1,27 @@
+package pe.mstrivial.models;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TriviaQuestionsEntity extends BaseEntity{/*
+    private static String DEFAULT_SQL = "SELECT * FROM trivialQuestions";
+
+    private List<TriviaQuestion> findByCriteria(String sql, QuestionsEntity questionsEntity, PeopleEntity peopleEntity1, PeopleEntity peopleEntity2){
+        List<TriviaQuestion> triviaQuestions = new ArrayList<>();
+        if (getConnection() != null{
+            try {
+                ResultSet resultSet = getConnection().createStatement().executeQuery(sql);
+                while (resultSet.next()){
+                    TriviaQuestion triviaQuestion = new TriviaQuestion(
+                            resultSet.getString("id"),
+
+                    )
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }*/
+}
