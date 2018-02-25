e2fe19f0ee78096ba45f6de703d5a77cd63392e1
Aaron Quiroz Hostia <1412955@utp.edu.pe>
2018-02-24 23:35:54 -0500

    Se creo la clase Company.

---
+++ src/pe/mstrivial/models/Company.java
@@ -0,0 +1,67 @@
+package pe.mstrivial.models;
+
+import java.util.Date;
+
+public class Company {
+    private int     id;
+    private String  name;
+    private String  ruc;
+    private Date    createDate;
+    private Date    modifyDate;
+
+    public Company() {
+    }
+
+    public Company(int id, String name, String ruc, Date createDate, Date modifyDate) {
+        this.setId(id);
+        this.setName(name);
+        this.setRuc(ruc);
+        this.setCreateDate(createDate);
+        this.setModifyDate(modifyDate);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Company setId(int id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Company setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getRuc() {
+        return ruc;
+    }
+
+    public Company setRuc(String ruc) {
+        this.ruc = ruc;
+        return this;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public Company setCreateDate(Date createDate) {
+        this.createDate = createDate;
+        return this;
+    }
+
+    public Date getModifyDate() {
+        return modifyDate;
+    }
+
+    public Company setModifyDate(Date modifyDate) {
+        this.modifyDate = modifyDate;
+        return this;
+    }
+}
