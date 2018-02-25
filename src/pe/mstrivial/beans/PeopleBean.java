c01690e44f67cc0e01f8609448d75428b2841ac4
davidestrada12 <35356255+davidestrada12@users.noreply.github.com>
2018-02-25 15:31:06 -0500

    Se modifico la configuracion de la clase PeopleBeans

--- src/pe/mstrivial/beans/PeopleBean.java
+++ src/pe/mstrivial/beans/PeopleBean.java
@@ -46,50 +46,50 @@ public class PeopleBean implements Serializable {
         this.person = person;
     }
 
-   // public int getId(){
-     //   return this.getCompany().getId();
+    // public int getId(){
+    //   return this.getCompany().getId();
     //}
 
-    public String getFirstName(){
+    public String getFirstName() {
         return this.getPerson().getFirstName();
     }
 
-    public String getLastNameP(){
+    public String getLastNameP() {
         return this.getPerson().getLastNameP();
     }
 
-    public String getLastNameM(){
+    public String getLastNameM() {
         return this.getPerson().getLastNameM();
     }
 
-    public String getCountry(){
+    public String getCountry() {
         return this.getPerson().getCountry();
     }
 
-    public String getEmail(){
+    public String getEmail() {
         return this.getPerson().getEmail();
     }
 
-    public String getUsername(){
+    public String getUsername() {
         return this.getPerson().getUsername();
     }
 
-    public String getPassword(){
+    public String getPassword() {
         return this.getPerson().getPassword();
     }
 
-    public Date getCreateDate(){
+    public Date getCreateDate() {
         return this.getPerson().getCreateDate();
     }
 
-    public Date getModifyDate(){
+    public Date getModifyDate() {
         return this.getPerson().getModifyDate();
     }
 
-    public String createPerson(){
-        service.createPerson(this.getCompany(),this.getFirstName(),this.getLastNameP(),
-                this.getLastNameM(),this.getCountry(),this.getEmail(),this.getUsername(),
-                this.getPassword(),this.getCreateDate(),this.getModifyDate());
+    public String createPerson() {
+        service.createPerson(this.getCompany(), this.getFirstName(), this.getLastNameP(),
+                this.getLastNameM(), this.getCountry(), this.getEmail(), this.getUsername(),
+                this.getPassword(), this.getCreateDate(), this.getModifyDate());
         return "success";
     }
 
@@ -103,9 +103,9 @@ public class PeopleBean implements Serializable {
         return "success";
     }
 
-    //public String updatePerson() {
-      //      service.updatePerson(this.getPerson());
-        //    return "success";
-    //}
+    public String updatePerson(CompaniesEntity companiesEntity) {
+          service.updatePerson(this.getPerson(), companiesEntity);
+        return "success";
 
+}
 }
